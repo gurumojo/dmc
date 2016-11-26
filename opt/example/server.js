709c719c86c3fc8ebc5646ea1a40dc4ae3362401
Matthew Larson <theguy@gurumojo.net>
2016-11-26 01:14:32 -0800

    redacted log messages

--- opt/example/server.js
+++ opt/example/server.js
@@ -1,6 +1,5 @@
 'use strict';
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
 const express = require('express');
 const expressSession = require('express-session');
 const favicon = require('serve-favicon');
@@ -11,16 +10,19 @@ const partial = require('lodash/partial');
 const pick = require('lodash/pick');
 const process = require('process');
 const readdir = require('fs').readdirSync;
+const RedisStore = require('connect-redis')(expressSession)
 
 const config = require('./etc/config');
 const logger = require('./lib/logger');
 const passport = require('./lib/session');
+const redact = require('./lib/redact');
 
-const DISCOVERY_BLACKLIST = ['Dockerfile', 'etc', 'lib', 'node_modules'];
+const DISCOVERY_BLACKLIST = ['Dockerfile', 'node_modules', 'etc', 'lib', 'var'];
 const FAVICON_PATH = `${__dirname}/etc/favicon.png`;
-const PORT = process.env.GURUMOJO_SERVICE_PORT || 80;
+const REDIS_HOSTNAME = process.env.GURUMOJO_REDIS_HOSTNAME || 'localhost';
 const REQUEST_WHITELIST = ['method', 'path', 'query', 'body', 'headers', 'sessionID'];
-const SECRET = process.env.GURUMOJO_SESSION_SECRET || 'y0uRbl00Dt4st3Slik3$yruP';
+const SERVICE_PORT = process.env.GURUMOJO_SERVICE_PORT || 80;
+const SESSION_SECRET = process.env.GURUMOJO_SESSION_SECRET || 'y0uRbl00Dt4st3Slik3$yruP';
 const STATIC_PATH = `${__dirname}/etc/public`;
 
 
@@ -37,7 +39,7 @@ function discover(type, array, value) {
 }
 
 function loop(level, flash) {
-	flash.forEach(message => logger[level]('example.message', message));
+	flash.forEach(message => logger[level]('example.message', redact(message)));
 }
 
 function message(request, response, next) {
@@ -53,12 +55,12 @@ function requestLogger(request, response, next) {
 			loop(method, response.locals[method]);
 		}
 	});
-	logger.info('example.request', pick(request, REQUEST_WHITELIST));
+	logger.info('example.request', redact(pick(request, REQUEST_WHITELIST)));
 	next();
 }
 
 function sessionLogger(request, response, next) {
-	logger.debug('example.session', request.user);
+	logger.debug('example.session', request.session);
 	next();
 }
 
@@ -69,6 +71,7 @@ function timestamp(request, response, next) {
 
 
 const service = express();
+service.disable('etag');
 service.disable('x-powered-by');
 
 service.use(datetime);
@@ -76,15 +79,21 @@ service.use(timestamp);
 
 //service.use(express.static(STATIC_PATH));
 service.use(favicon(FAVICON_PATH));
-service.use(cookieParser());
+
 service.use(bodyParser.json());
-service.use(bodyParser.urlencoded({extended: true}));
+service.use(bodyParser.urlencoded({extended: false}))
+
 
 service.use(flash());
 service.use(expressSession({
 	resave: false,
 	saveUninitialized: false,
-	secret: SECRET
+	secret: SESSION_SECRET,
+	store: new RedisStore({
+		host: REDIS_HOSTNAME,
+		prefix: 'session:',
+		ttl: 3600
+	})
 }));
 
 service.use(passport.initialize());
@@ -112,9 +121,9 @@ service.get('/*', (request, response) => {
 });
 
 //logger.level('debug');
-service.listen(PORT, () => {
+service.listen(SERVICE_PORT, () => {
 	logger.info('init', {
 		application: config.application,
-		uri: `http://${hostname}:${PORT}/`
+		uri: `http://${hostname}:${SERVICE_PORT}/`
 	});
 });
